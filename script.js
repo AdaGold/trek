5aff5b92514897dd567aab2db3db6a9107bb3d4d
laurenfb <laurenfriesbrink@gmail.com>
2016-11-23 13:11:33 -0800

    all trips visible (not through a button yet) - did this yesterday

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
 
-  
+  var url = "https://trektravel.herokuapp.com/trips";
+
+  var multiCallback = function(listOfTrips) {
+    // alert("hi we're in the function!");
+    for (var i = 0; i < listOfTrips.length; i++) {
+      $('#trip-list').append("<li>" + listOfTrips[i].name + "</li>")
+    };
+  };
+
+  $.get(url, multiCallback);
+
 }); // end of document dot ready
