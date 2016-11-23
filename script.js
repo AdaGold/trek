ee0cd4910ca2d7228f156b56d30ad7fe7a72ed1d
laurenfb <laurenfriesbrink@gmail.com>
2016-11-23 14:10:39 -0800

    added links, they currently go to the API though.

--- script.js
+++ script.js
@@ -5,16 +5,21 @@ $(document).ready(function() {
   var multiCallback = function(listOfTrips) {
     // alert("hi we're in the function!");
     for (var i = 0; i < listOfTrips.length; i++) {
-      $('#trip-list').append("<li>" + listOfTrips[i].name + "</li>")
+      $('#trip-list').append("<li><a href='" + url + '/' + listOfTrips[i].id + "'>" + listOfTrips[i].name + "</a></li>")
     };
   };
 
+  //  $('#pets').append('<li><a href=' + url + '/' + response[i].id + '>' + response[i].name + '</a></li>');
 
   var loadTrips = function() {
     $('#load').click(function() {
       $.get(url, multiCallback);
       buttonText();
     });
+  };
+
+  var clickTrip = function() {
+
   };
 
   loadTrips();
