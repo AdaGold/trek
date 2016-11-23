94004433a725ddc12f080630d31fb358839df571
laurenfb <laurenfriesbrink@gmail.com>
2016-11-23 15:12:34 -0800

    helper methods to nicely display weeks and dollars

---
+++ helper.js
@@ -0,0 +1,15 @@
+var weekHelper = function(weeks) {
+  if (weeks == 1) {
+    return "WEEK"
+  } else {
+    return "WEEKS"
+  }
+};
+
+var dollarHelper = function(weeks) {
+  if (weeks == 1) {
+    return "WEEK"
+  } else {
+    return "WEEKS"
+  }
+};
