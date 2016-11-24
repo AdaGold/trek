357f3222de2faa9932a3dd3efa34f4faf0567e0c
Elle Vargas <vargas.elle@gmail.com>
2016-11-23 16:29:38 -0800

    wave 3 stuff now working, css still needs some help

--- scripts.js
+++ scripts.js
@@ -1,32 +1,72 @@
 $(document).ready(function() {
 
-  // Which URL do we want to 'get'?
   var url = 'https://trektravel.herokuapp.com/trips';
 
-  // What do we want to happen when we get our response?
-  var successCallback = function (response) {
+  var tripsCallback = function (response) {
     for (var i=0; i < response.length; i++) {
       $('#trips').append("<h2><a href=" + url + "/" + response[i].id + " > " + response[i].name + "</a></h2>");
     }
   };
-  $('#load').on('click', function() {
-    $.get(url,successCallback);
-  })
-})
 
-// $('#trips').on('click', 'a', function(e) {
-//   e.preventDefault();
-//
-//   $('#package').show();
-//   var tripUrl = $(this).attr('href');
-//
-//   $.get(tripUrl, function(trip) {
-//     $('#name').text(trip.name);
-//     $('#continent').text(trip.continent);
-//     $('#about').text(trip.about);
-//   }).always(function () {
-//     $('#message').text("Something happened")
-//   }).fail(function(){
-//     alert("Failed.");
-//   })
-// });
+  $('#load').on('click', function() {
+    $.get(url, tripsCallback);
+  })
+
+  $('#trips').on('click', 'a', function(e) {
+    e.preventDefault();
+
+
+    $('#package').show();
+    var tripUrl = $(this).attr('href');
+
+    $.get(tripUrl, function(trip) {
+      $('#name').text(trip.name + " (" + trip.continent + ")" );
+      $('#category').text("Category: " + trip.category);
+      $('#weeks').text("Trip Length: " + trip.weeks + " weeks");
+      $('#cost').text("Trip Cost: $" + trip.cost);
+      $('#id').html("TREK ID: " + "<span>" + trip.id +  "</span>");
+      $('#about').text(trip.about);
+    }).always(function () {
+    }).fail(function(){
+      alert("Failed.");
+    })
+  });
+
+
+  // var reservationsCallback = function (response) {
+  //   for (var i=0; i < response.length; i++) {
+  //     $('#reservations').append("<h2><a href=" + url + "/trips/" + response[i].id + " > " + response[i].name + "</a></h2>");
+  //   }
+  // };
+
+  // how is .click in comparison?
+
+  $('form').submit(function(e) {
+    e.preventDefault();
+
+    var form_url = $(this).attr('action')
+    var url =  form_url + "/" + $('#id span').text() + "/reserve";
+    var formData = $(this).serialize();
+
+    $.post(url, formData, function(response){
+      console.log(response);
+      $('#message').html('Your reservation is good to go!');
+    })
+      // $('#reservation_form').hide();
+
+
+      // .fail(function(){
+      // $('#message').html('<p>Adding trip failed</p>');
+    });
+  });
+
+  //     $.ajax({
+  //     type: "POST",
+  //     data: formData,
+  //     cache: false,
+  //     url: url,
+  //     success: function(data)
+  //     {
+  //       console.log(data)
+  //     }
+  // });
