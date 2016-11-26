3f77a21f2086bd613b2acf87a14d10052251be25
jm-rives <joanna.rives@gmail.com>
2016-11-26 15:45:35 -0800

    WAVE 2 complete

--- scripts.js
+++ scripts.js
@@ -1,26 +1,43 @@
 $(document).ready(function(){
   
-  var tripUrl = 'https://trektravel.herokuapp.com/trips';
+  var tripsUrl = 'https://trektravel.herokuapp.com/trips';
+
+  var tripUrl = 'https://trektravel.herokuapp.com/trips/4';
+
   // show list of trips on click
   // variable extraction refactoring
-  var dataArrivedCallback = function(data){
+  var onDataArrivedCallback = function(data){
 
     for (i = 0; i < data.length; i ++){
-      var tripNames = '<h3><a href=' + tripUrl + '/' + data[i].id + '>' + data[i].name + '</a></h3>'
+      var tripNames = '<h3><a href=' + tripsUrl + '/' + data[i].id + '>' + data[i].name + '</a></h3>'
   
       $('#trips').append(tripNames);
     };
   };
 
   // variable extraction refactoring
-  var clickCallback = function(){
-    $.get(tripUrl, dataArrivedCallback);
+  var onLoadClicked = function(){
+    $.get(tripsUrl, onDataArrivedCallback);
   };
-
-  $('#load').on('click', clickCallback);
-
+  
+  $('#load').on('click', onLoadClicked);
 
   console.log('Wave 2 starts here');
 // show details of one trip on
+  var onTripDetailsArrived = function(trip){
+      $('#id').text(trip.id);
+      $('#name').text(trip.name);
+      $('#continent').text(trip.continent);
+      $('#about').text(trip.about);
+      $('#category').text(trip.category);
+      $('#weeks').text(trip.weeks);
+      $('#cost').text(trip.cost);
+    };
 
+  var onTripClicked = function(event){
+    event.preventDefault();
+    $.get(this.href, onTripDetailsArrived);
+  };
+
+  $('#trips').on('click', 'a', onTripClicked);
 })
