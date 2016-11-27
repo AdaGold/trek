7e88ba65a658b38f4230b14e3697d749ba091795
jm-rives <joanna.rives@gmail.com>
2016-11-26 19:04:51 -0800

    ADD reservation functioning

--- scripts.js
+++ scripts.js
@@ -1,43 +1,96 @@
-$(document).ready(function(){
-  
+$(document).ready(function() {
+
   var tripsUrl = 'https://trektravel.herokuapp.com/trips';
 
-  var tripUrl = 'https://trektravel.herokuapp.com/trips/4';
+
 
   // show list of trips on click
   // variable extraction refactoring
-  var onDataArrivedCallback = function(data){
+  var onDataArrivedCallback = function(data) {
 
-    for (i = 0; i < data.length; i ++){
+    for (i = 0; i < data.length; i++) {
       var tripNames = '<h3><a href=' + tripsUrl + '/' + data[i].id + '>' + data[i].name + '</a></h3>'
-  
+
       $('#trips').append(tripNames);
     };
   };
 
   // variable extraction refactoring
-  var onLoadClicked = function(){
+  var onLoadClicked = function() {
     $.get(tripsUrl, onDataArrivedCallback);
   };
-  
+
   $('#load').on('click', onLoadClicked);
 
-  console.log('Wave 2 starts here');
-// show details of one trip on
-  var onTripDetailsArrived = function(trip){
-      $('#id').text(trip.id);
-      $('#name').text(trip.name);
-      $('#continent').text(trip.continent);
-      $('#about').text(trip.about);
-      $('#category').text(trip.category);
-      $('#weeks').text(trip.weeks);
-      $('#cost').text(trip.cost);
-    };
 
-  var onTripClicked = function(event){
+
+
+  // show details of one trip on
+  var onTripDetailsArrived = function(trip) {
+    $('#id').text(trip.id);
+    $('#name').text(trip.name);
+    $('#continent').text(trip.continent);
+    $('#about').text(trip.about);
+    $('#category').text(trip.category);
+    $('#weeks').text(trip.weeks);
+    $('#cost').text(trip.cost);
+    $('#a_trip').show();
+  };
+
+  var onTripClicked = function(event) {
     event.preventDefault();
     $.get(this.href, onTripDetailsArrived);
   };
 
   $('#trips').on('click', 'a', onTripClicked);
+
+  // Wave 3 starts here
+
+  $('#submit').on('click', function(event) {
+    event.preventDefault();
+    var name = $('#person_name').val();
+    var age = $('#age').val();
+    var email = $('#mail').val();
+
+    var personalData = {
+      name: name,
+      age: age,
+      email: email
+    }
+
+    var callback = function() {
+      console.log("Success!");
+    };
+    var tripId = $('#id').text(); 
+    var reservationUrl = "https://trektravel.herokuapp.com/trips/" + tripId + "/reserve"
+
+    $.post(reservationUrl, personalData, callback);
+  
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
