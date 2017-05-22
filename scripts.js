0d86fc17c04e463f31897dd3460b10f41e16f0b3
Jamie Pilgrim <hello@jamiepilgrim.com>
2017-05-22 08:20:25 -0700

    trips view, code for other jquery actions

--- scripts.js
+++ scripts.js
@@ -1,41 +1,65 @@
 $(document).ready(function(){
   // URL of the API
   var url = 'https://trektravel.herokuapp.com/trips';
-
+  // change api to have australiasia be australia
+  var continents = ['asia', 'africa', 'europe', 'south america', 'north america', 'antartica', 'australasia']
 
   var successCallback = function(response) {
     for (var i = 0; i < response.length; i++) {
-      if(response[i].continent !== null){
+      if(response[i].continent === "Asia"){
         $("#trips ul").append(
-          "<li><h3><a href=" + url +  "/" + response[i].id + ">"
-          + response[i].name + "</a></h3></li>"); //end append
+          "<li data-equalizer-watch class='column'><div><h3><a href=" + url +  "/" + response[i].id + ">"
+          + response[i].name + "</a></h3></div></li>"); //end append
         }
-    } // end for
-  }; // end successCallback
+    } //for
+    matchHeight();
+  }; //successCallback
 
   $('#see-trips').click(function() {
     $.get(url, successCallback);
   });
 
 
-  $('#see-trips').on('click', 'a', function(e) { //e is short for 'event'
+  $('#see-trips-by-continent').on('click', 'a', function(e) {
+    var continentUrl = url + '/trips/continent?query=' + $(this).data('continent');
+    $.get(continentUrl, successCallback);
+  });
+
+  $('#see-reservations').on('click', function(e) {
+    // Popup with form to get email
+  });
+
+  $('#see-reservations-by-email').on('click', function(e) {
+    e.prevetDefault();
+    $('#reservatios-form').hide();
+    $('#reservations').show();
+    // Get
+    var reservationsUrl = $(this).attr('');
+    $.get(reservationsUrl, successCallback);
+  });// see-reservations-by-email.click
+
+
+  $('#see-trips').on('click', 'a', function(e) {
     e.preventDefault();
-
     $('#trip').show();
-
-    var showTripUrl = $(this).attr('href');  // attr() = attributes
-    var tripID = $.get(showTripUrl, tripInfomation).fail(function() {
+    var tripUrl = $(this).attr('href');
+    var tripID = $.get(tripUrl, tripInfomation).fail(function() {
       alert("Page Not Found");
-    });
-  });
+    });//get tripURL .fail
+  }); // see-trips.click
 
 
+  var matchHeight = function(){
+    $("[data-equalize]").each(function() {
 
+      var parentRow = $(this),
+          childrenCols = $(this).find("[data-equalizer-watch]"),
+          childHeights = childrenCols.map(function(){ return $(this).height(); }).get(),
+          tallestChild = Math.max.apply(Math, childHeights);
 
-  $('#see-reservations').click(function() {
-    $.get(url + '/3/reservations', successCallback);
-  });
+      childrenCols.css('height', tallestChild);
 
+    }); //data-equalize.each
+  }; // matchHeight()
 
-
-});
+}); // document.ready
