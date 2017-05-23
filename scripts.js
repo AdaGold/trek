465c2067c7c01d1fc9447b4740c87916d5b32675
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2017-05-22 17:01:34 -0700

    show reservations by email, trip info, reservation form

--- scripts.js
+++ scripts.js
@@ -2,48 +2,83 @@ $(document).ready(function(){
   // URL of the API
   var url = 'https://trektravel.herokuapp.com/trips';
   // change api to have australiasia be australia
-  var continents = ['asia', 'africa', 'europe', 'south america', 'north america', 'antartica', 'australasia']
+  var continents = ['Asia', 'Africa', 'Europe', 'South America', 'North America', 'Antartica', 'Australasia'];
 
-  var successCallback = function(response) {
+  var budgets = [500, 1000, 1500, 2000, 2500, 3000, 3500, 4000];
+
+  for (i = 0; i < continents.length; i++) {
+    $("#see-trips-by-continent").append("<li><a class='success button' href=" + continents[i] + ">" + continents[i] + "</a></li>");
+  }
+
+
+  for (i = 0; i < budgets.length; i++) {
+    $("#see-trips-by-budget").append("<li><a class='success button' href=" + budgets[i] + ">" + budgets[i] + "</a></li>");
+  }
+
+  var showTrips = function(response) {
+    $("#trips ul").html(" ");
     for (var i = 0; i < response.length; i++) {
-      if(response[i].continent === "Asia"){
         $("#trips ul").append(
-          "<li data-equalizer-watch class='column'><div><h3><a href=" + url +  "/" + response[i].id + ">"
+          "<li data-equalizer-watch class='column see-trip'><div><h3><a href=" + response[i].id + ">"
           + response[i].name + "</a></h3></div></li>"); //end append
-        }
     } //for
     matchHeight();
-  }; //successCallback
+  }; //showTrips
+
+
+  var tripShow = function(response) {
+    $('#trip-name').html(response.name);
+  }; //tripShow()
 
   $('#see-trips').click(function() {
-    $.get(url, successCallback);
+    $.get(url, showTrips);
   });
 
 
   $('#see-trips-by-continent').on('click', 'a', function(e) {
-    var continentUrl = url + '/trips/continent?query=' + $(this).data('continent');
-    $.get(continentUrl, successCallback);
+    e.preventDefault();
+    console.log("hey");
+    var continentUrl = url + '/continent?query=' + $(this).attr('href');
+    $.get(continentUrl, showTrips);
+  });
+
+  $('#reserve-form').submit(function(event) {
+    event.preventDefault();
+    $('#messages').append("<h3>Reservation Made!</h3");
+  });
+
+  $('#close-trip').on('click', function(e){
+    e.preventDefault();
+    $('#messages').html(" ");
+    $('#trip').hide();
   });
 
   $('#see-reservations').on('click', function(e) {
-    // Popup with form to get email
+    $('#see-reservations-by-email').show();
   });
 
-  $('#see-reservations-by-email').on('click', function(e) {
-    e.prevetDefault();
-    $('#reservatios-form').hide();
+  $('#form-test').submit(function(event) {
+    event.preventDefault();
+    var email = $('#form-test').serializeArray()[0]["value"];
+    $('#form-test').hide();
     $('#reservations').show();
-    // Get
-    var reservationsUrl = $(this).attr('');
-    $.get(reservationsUrl, successCallback);
-  });// see-reservations-by-email.click
+    var reservationUrl = "https://trektravel.herokuapp.com/reservations"
+    $.post(reservationUrl, {email: email}, function(data){
+      for(i=0; i < data.length; i++){
+        $('#reservations ul').append("<li><a href="+ data[i].id + ">" + data[i].name + "</li>");
+      }
+    }).fail(function() {
+      alert("Page Not Found");
+    });
+  });
 
 
-  $('#see-trips').on('click', 'a', function(e) {
+  $('#trips ul').on('click', 'a', function(e) {
     e.preventDefault();
     $('#trip').show();
-    var tripUrl = $(this).attr('href');
-    var tripID = $.get(tripUrl, tripInfomation).fail(function() {
+    var tripId = $(this).attr('href');
+    var tripUrl = url + '/' + tripId;
+    var trip = $.get(tripUrl, tripShow).fail(function() {
       alert("Page Not Found");
     });//get tripURL .fail
   }); // see-trips.click
